4691485b10adb2c842e03cf133dd81a03102af04
Revanth <revanth.g25@gmail.com>
2022-04-06 03:42:56 +0200

    NA

--- Pokedex/Program.cs
+++ Pokedex/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pokedex Api", Version = "v1" });
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
